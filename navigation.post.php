295097569a2c5c10c20d5824abf3aaf74a7013f9
Matt Read <matt@mattread.com>
2012-09-20 21:53:17 -0400

    escape title tag for navigation links next/previous posts

--- navigation.post.php
+++ navigation.post.php
@@ -1,8 +1,8 @@
 <nav>
 <?php if ( isset($next) && $next ) : ?>
-<a href="<?php echo $next->permalink; ?>" title="<?php echo $next->title; ?>">&larr; Next Post</a> &nbsp;|&nbsp;
+<a href="<?php echo $next->permalink; ?>" title="<?php echo htmlentities($next->title); ?>">&larr; Next Post</a> &nbsp;|&nbsp;
 <?php endif; ?>
 <?php if ( isset($previous) && $previous ) : ?>
-<a href="<?php echo $previous->permalink; ?>" title="<?php echo $previous->title; ?>"">Previous Post &rarr;</a>
+<a href="<?php echo $previous->permalink; ?>" title="<?php echo htmlentities($previous->title); ?>">Previous Post &rarr;</a>
 <?php endif; ?>
 </nav>
