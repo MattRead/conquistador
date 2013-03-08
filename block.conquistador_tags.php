03574d6f89709a005a715003f02118107f04ba0e
Matt Read <matt@mattread.com>
2013-03-08 17:48:50 -0500

    add tags list block

---
+++ block.conquistador_tags.php
@@ -0,0 +1,8 @@
+<?php namespace Habari; ?>
+<nav>
+	<p class="meta tags">
+		<?php if ( isset($post) && count((array)$post->tags) ) : ?>
+		Tags: <?php echo $post->tags_out; ?>
+		<?php endif; ?>
+	</p>
+</nav>
