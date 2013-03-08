882b1a5fb459f36b88aaf11149a018e299aa4059
Matt Read <matt@mattread.com>
2013-03-08 17:14:24 +0000

    pingback style updates

--- pingback.php
+++ pingback.php
@@ -1,9 +1,9 @@
 <?php //namespace Habari; ?>
 		<article id="comment-<?php echo $content->id; ?>" class="comment type-<?php echo $content->typename; ?> status-<?php echo $content->statusname; ?>">
             <p>
-                <a href="<?php echo $content->url; ?>" rel="external">
-                    <?php echo $content->name != NULL ? $content->name : $content->url; ?>
-                </a>,
+				<a href="<?php echo $content->url; ?>" rel="external">
+                    <?php echo $content->name != NULL ? trim($content->name, "\t\n\r\0\x0B\x160") : trim($content->url); ?>
+                </a><br>
                 <time class="meta" datetime="<?php echo $content->date->format('c'); ?>"><?php echo $content->date_out; ?></time>
             </p>
         </article>
