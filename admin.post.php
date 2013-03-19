82f174ef5c7fae45f83a4be6d797c3d798ca6453
Matt Read <matt@mattread.com>
2013-03-19 15:19:19 +0000

    reduce the admin templates to one (admin.content)

--- admin.post.php
+++
@@ -1,7 +0,0 @@
-<?php //namespace Habari; ?>
-<?php if ( User::identify()->loggedin ) : ?>
-<div class="admin">
-	<a class="editlink" href="<?php echo $content->editlink; ?>">Edit</a>
-</div>
-<?php endif; ?>
-
