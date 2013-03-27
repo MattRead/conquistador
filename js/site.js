a5fceb7f1ab0e8c17553698b564f6e58e5625433
Matt Read <matt@mattread.com>
2013-03-27 00:02:40 +0000

    elastic videos

--- js/site.js
+++ js/site.js
@@ -66,4 +66,36 @@ $(document).ready(function() {
 	if ( CONQUISTADOR_USE_FANCYBOX ) {
 		$('a.fancybox').fancybox();
 	}
+$(function() {
+    
+    var $allVideos = $("iframe[src^='http://player.vimeo.com'], iframe[src^='http://www.youtube.com'], object, embed"),
+    $fluidEl = $("div");
+	    	
+	$allVideos.each(function() {
+	
+	  $(this)
+	    // jQuery .data does not work on object/embed elements
+	    .attr('data-aspectRatio', this.height / this.width)
+	    .removeAttr('height')
+	    .removeAttr('width');
+	
+	});
+	
+	$(window).resize(function() {
+	
+	  var newWidth = $fluidEl.width();
+	  $allVideos.each(function() {
+	  
+	    var $el = $(this);
+	    $el
+	        .width(newWidth)
+	        .height(newWidth * $el.attr('data-aspectRatio'));
+	  
+	  });
+	
+	}).resize();
+
 });
+
+});
+
