d7eeb9541b8894d7a477b72ec7083dec12bcc9a0
Matt Read <matt@mattread.com>
2013-04-09 17:47:59 +0000

    style at-comment-reply

--- comment.php
+++ comment.php
@@ -5,7 +5,7 @@
 		<p>
 			<img class="gravatar" src="http://www.gravatar.com/avatar/<?php echo md5(strtolower($content->email)); ?>?d=identicon&s=60&r=g" alt="Gravatar for <?php echo $content->name; ?>">
 			<a href="<?php echo $content->url; ?>" rel="external"><?php echo $content->name != 'Anonymous' ? $content->name : $content->url; ?></a>
-				(<a href="#comment-<?php echo $content->id; ?>" title="Permanent link to this comment">#<?php echo $content->id; ?></a>)<br>
+				<small>(<a href="#comment-<?php echo $content->id; ?>" title="Permanent link to this comment">#<?php echo $content->id; ?></a>)</small><br>
 			<time datetime="<?php echo $content->date->format('c'); ?>"><?php echo $content->date_out; ?></time>
 		</p>
 		</header>
