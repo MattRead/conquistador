94ebb5fc9913b8a672283935cfb086568f24acf9
Matt Read <matt@mattread.com>
2013-04-09 01:26:38 +0000

    add blogroll list template

--- list.blogroll.php
+++ list.blogroll.php
@@ -1,7 +1,8 @@
 <?php namespace Habari; ?>
 <li id="post-<?php echo $content->id; ?>" class="<?php echo $content->css_class(); ?>">
 <time datetime="<?php echo $content->modified->format('c'); ?>">
-    <?php echo $content->modified_out_short; ?>
+	&harr; <?php echo $content->modified_out_short; ?>
 </time>
-<span><a href="<?php echo $content->permalink; ?>"><?php echo $content->title_out; ?></a></span>
+<span><a href="<?php echo $content->permalink; ?>" rel="<?php echo $content->realtionship; ?>"><?php echo $content->title_out; ?></a>
+<span class="meta"><?php echo $content->xfn_relationships; ?></span></span>
 </li>
