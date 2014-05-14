18cc9e1f18de3743378052615cd71573a70a1c3e
Matt Read <matt@mattread.com>
2014-05-14 19:29:26 +0000

    ponies bad

--- 404.php
+++ 404.php
@@ -5,10 +5,7 @@
     <header>
 	<h1>Page Not Found</h1>
     </header>
-	<p style="text-align:center">
-		<q cite="http://en.wikipedia.org/wiki/Marvin_the_Paranoid_Android">Funny, how just when you think life can't possibly get any worse it suddenly does.</q>
-		<img class="center" style="border:none; box-shadow:none; background:none;" src="<?php Site::out_url('theme') ?>/images/marvin.png">
-	</p>
+	<p style="text-align:center">Something has gone <b>horribly</b> wrong!</p>
 </article>
 
 <?php echo $theme->display('footer'); ?>
