31c8d0f01db71424e60c552dc6aaa45885ad77ae
Matt Read <matt@mattread.com>
2014-09-29 19:55:21 -0400

    code clean up

--- adminbar.php
+++ adminbar.php
@@ -1,37 +1,37 @@
 <?php if ($user = User::identify()->loggedin) : ?>
-	<ul class="admin site">
+<ul class="admin site">
 
-		<?php if (Controller::get_action() == 'display_post' && isset($theme->post) && ACL::access_check($theme->post->get_access($user), 'edit')) : ?>
-			<li><a href="<?php echo $post->editlink; ?>" title="Edit this post"><i
-						class="icon-pencil"></i><b>Edit</b></a></li>
-		<?php endif; ?>
+	<?php if (Controller::get_action() == 'display_post' && isset($theme->post) && ACL::access_check($theme->post->get_access($user), 'edit')) : ?>
+		<li><a href="<?php echo $post->editlink; ?>" title="Edit this post"><i
+					class="icon-pencil"></i><b>Edit</b></a></li>
+	<?php endif; ?>
 
-		<?php if (isset($user_draft_count) && $user_draft_count) : ?>
-			<li>
-				<a href="<?php URL::out('display_posts', 'page=posts&status=1'); ?>"
-				   title="<?php echo $user_draft_count; ?> Drafts"><i
-						class="icon-file4"></i><span><?php echo $user_draft_count; ?></span><b>Drafts</b></a>
-			</li>
-		<?php endif; ?>
-
-		<?php if (isset($unapproved_comment_count) && $unapproved_comment_count) : ?>
-			<li>
-				<a href="<?php URL::out('display_comments', 'page=comments&status=unapproved'); ?>"
-				   title="<?php echo $unapproved_comment_count; ?> comments waiting moderation"><i
-						class="icon-bubble"></i><span><?php echo $unapproved_comment_count; ?></span><b>Moderation</b></a>
-			</li>
-		<?php endif; ?>
-		<?php if (isset($spam_comment_count) && $spam_comment_count) : ?>
-			<li>
-				<a href="<?php URL::out('display_comments', 'page=comments&status=spam'); ?>"
-				   title="<?php echo $spam_comment_count; ?> spam comments"><i
-						class="icon-spam"></i><span><?php echo $spam_comment_count; ?></span><b>Spam</b></a>
-			</li>
-		<?php endif; ?>
-
+	<?php if (isset($user_draft_count) && $user_draft_count) : ?>
 		<li>
-			<a href="<?php echo URL::get('display_dashboard'); ?>"><i class="icon-home2" title="Dashboard"></i><b>Dashboard</b></a>
+			<a href="<?php URL::out('display_posts', 'page=posts&status=1'); ?>"
+			   title="<?php echo $user_draft_count; ?> Drafts"><i
+					class="icon-file4"></i><span><?php echo $user_draft_count; ?></span><b>Drafts</b></a>
 		</li>
+	<?php endif; ?>
 
-	</ul>
+	<?php if (isset($unapproved_comment_count) && $unapproved_comment_count) : ?>
+		<li>
+			<a href="<?php URL::out('display_comments', 'page=comments&status=unapproved'); ?>"
+			   title="<?php echo $unapproved_comment_count; ?> comments waiting moderation"><i
+					class="icon-bubble"></i><span><?php echo $unapproved_comment_count; ?></span><b>Moderation</b></a>
+		</li>
+	<?php endif; ?>
+	<?php if (isset($spam_comment_count) && $spam_comment_count) : ?>
+		<li>
+			<a href="<?php URL::out('display_comments', 'page=comments&status=spam'); ?>"
+			   title="<?php echo $spam_comment_count; ?> spam comments"><i
+					class="icon-spam"></i><span><?php echo $spam_comment_count; ?></span><b>Spam</b></a>
+		</li>
+	<?php endif; ?>
+
+	<li>
+		<a href="<?php echo URL::get('display_dashboard'); ?>"><i class="icon-home2" title="Dashboard"></i><b>Dashboard</b></a>
+	</li>
+
+</ul>
 <?php endif; ?>
--- block.conquistador_navigation.php
+++ block.conquistador_navigation.php
@@ -1,26 +1,26 @@
 <?php //namespace Habari; ?>
 <?php if (isset($content->next) || isset($content->previous) || isset($content->comments_disabled)) : ?>
-	<nav class="posts">
-		<p class="meta">
-			<?php if ($content->comment_count): ?>
-				<a href="#comments" title="Read Comments"><i class="icon-"></i> <?php echo $content->comment_count; ?>
-					Comments</a> &nbsp;|&nbsp;
-			<?php endif; ?>
-			<?php if (isset($content->comments_disabled) && !$content->comments_disabled) : ?>
-				<a href="#respond" title="Leave a Comment"><i class="icon-"></i> Leave a Comment</a>
-			<?php endif; ?>
-			<?php if ((isset($content->comments_disabled) && !$content->comments_disabled) || $content->comment_count) : ?>
-				<br>
-			<?php endif; ?>
-			<?php if (isset($content->next) && $content->next) : ?>
-				<a href="<?php echo $content->next->permalink; ?>"
-				   title="<?php echo htmlentities($content->next->title); ?>"><i class="icon-arrow-left2"></i> Next Post</a> &nbsp;|&nbsp;
-			<?php endif; ?>
-			<?php if (isset($content->previous) && $content->previous) : ?>
-				<a href="<?php echo $content->previous->permalink; ?>"
-				   title="<?php echo htmlentities($content->previous->title); ?>">Previous Post <i
-						class="icon-arrow-right2"></i></a>
-			<?php endif; ?>
-		</p>
-	</nav>
+<nav class="posts">
+	<p class="meta">
+		<?php if ($content->comment_count): ?>
+			<a href="#comments" title="Read Comments"><i class="icon-"></i> <?php echo $content->comment_count; ?>
+				Comments</a> &nbsp;|&nbsp;
+		<?php endif; ?>
+		<?php if (isset($content->comments_disabled) && !$content->comments_disabled) : ?>
+			<a href="#respond" title="Leave a Comment"><i class="icon-"></i> Leave a Comment</a>
+		<?php endif; ?>
+		<?php if ((isset($content->comments_disabled) && !$content->comments_disabled) || $content->comment_count) : ?>
+			<br>
+		<?php endif; ?>
+		<?php if (isset($content->next) && $content->next) : ?>
+			<a href="<?php echo $content->next->permalink; ?>"
+			   title="<?php echo htmlentities($content->next->title); ?>"><i class="icon-arrow-left2"></i> Next Post</a> &nbsp;|&nbsp;
+		<?php endif; ?>
+		<?php if (isset($content->previous) && $content->previous) : ?>
+			<a href="<?php echo $content->previous->permalink; ?>"
+			   title="<?php echo htmlentities($content->previous->title); ?>">Previous Post <i
+					class="icon-arrow-right2"></i></a>
+		<?php endif; ?>
+	</p>
+</nav>
 <?php endif; ?>
